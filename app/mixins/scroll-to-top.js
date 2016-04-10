9d64f700a783e98a659b3d5bff3e5ac2e586057f
jbandura <jacek.bandura@netguru.pl>
2016-04-10 17:46:42 +0200

    add scroll to top when transitioning between routes

---
+++ app/mixins/scroll-to-top.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+const { Mixin } = Ember;
+
+export default Mixin.create({
+  activate() {
+    this._super(...arguments);
+    window.scrollTo(0,0);
+  }
+});
--- app/pods/docs/validators-reference/route.js
+++ app/pods/docs/validators-reference/route.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import ScrollToTopMixin from 'ember-legit-forms-docs/mixins/scroll-to-top';
 
 const { Route, inject: { service }} = Ember;
 
-export default Route.extend({
+export default Route.extend(ScrollToTopMixin, {
   store: service(),
 
   model() {

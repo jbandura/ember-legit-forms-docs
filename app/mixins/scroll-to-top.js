259b04909f3aada28da3f61d0d2179308dccbe6d
jbandura <jacek.bandura@netguru.pl>
2016-05-01 23:15:39 +0200

    make mobile nav hide on transitions + adjust index page for mobile

--- app/mixins/scroll-to-top.js
+++ app/mixins/scroll-to-top.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
-const { Mixin } = Ember;
+const { Mixin, inject: { service } } = Ember;
 
 export default Mixin.create({
+  mobileTrigger: service(),
   activate() {
     this._super(...arguments);
     window.scrollTo(0,0);
+    this.set('mobileTrigger.mobileNavExpanded', false);
   }
 });
